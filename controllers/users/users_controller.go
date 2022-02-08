7719d60bc9f6f63c6045ea3f464fb69650408433
aryabaguss <69476159+aryabaguss@users.noreply.github.com>
2022-02-08 17:24:24 +0700

    4

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,13 +2,27 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github/aryabaguss/bookstore_users-api/domain/users"
+	"github/aryabaguss/bookstore_users-api/services"
+	"github/aryabaguss/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
-func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
-}
-
 func CreateUser(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadrequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+
+}
+func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me!")
 }
--- services/users_service.go
+++ services/users_service.go
@@ -1,2 +1,15 @@
 package services
 
+import (
+	"github/aryabaguss/bookstore_users-api/domain/users"
+	"github/aryabaguss/bookstore_users-api/utils/errors"
+	"net/http"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+
+	return &user, &errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+}
